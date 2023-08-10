b405f32054fcad0b7958682a2ef540ac040b169e
Alaa Eddine <alaaelhannaoui@proton.me>
2023-08-10 10:30:40 -0700

    size of variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - function
+ *Return: 0
+ */
+int main(void)
+{
+	pritnf("Size of a char: %d byte(s)", sizeof(char));
+	pritnf("Size of a int: %d byte(s)", sizeof(int));
+	pritnf("Size of a long int: %d byte(s)", sizeof(lont int));
+	pritnf("Size of a long long int: %d byte(s)", sizeof(long long int));
+	pritnf("Size of a float: %d byte(s)", sizeof(float));
+	return (0);
+}
