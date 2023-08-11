15f876452c99b656fe1603422c852eb66670f1f5
Alaa Eddine <alaaelhannaoui@proton.me>
2023-08-11 15:36:12 -0700

    the last advanced task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - function
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+	char j, k;
+
+	for (k = 0; k < 99; k++)
+	{
+		for (j = k + 1; j < 100; j++)
+		{
+			putchar(k / 10 + '0');
+			putchar(k % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (k == 98 && j == 99)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
