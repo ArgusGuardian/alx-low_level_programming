d306fe0aa32c9f087e250f7b5b9f21877c8224ab
Alaa Eddine <alaaelhannaoui@proton.me>
2023-08-11 15:12:45 -0700

    print comb of 2

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - function
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+	char i, j;
+
+	for (j = '0'; j <= '8'; j++)
+	{
+		for (i = j; i <= '9'; i++)
+		{
+			if (i == j)
+			{
+				continue;
+			}
+			putchar(j);
+			putchar(i);
+			if (i == '9' && j == '8')
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
