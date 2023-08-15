4d246afe77d4e6b9dcd56ae78049cf7f84577e65
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-15 12:29:59 +0100

    print alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Function
+ * Description: prints the alpha
+ */
+void print_alphabet(void)
+{
+	char A = 97;
+
+	while (A != 123)
+	{
+		_putchar(A);
+		A++;
+	}
+	_putchar('\n');
+}
