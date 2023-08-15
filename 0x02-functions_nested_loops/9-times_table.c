565937e2f7ac0ab3a29f375831ca03f0ca79e754
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-15 13:43:14 +0100

    times 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * jack_bauer - Function
+ * Description: prints all the hours and minutes
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			int mul;
+
+			mul = i * j;
+			if (mul > 9)
+			{
+
+				_putchar((mul / 10) + '0');
+				_putchar((mul % 10) + '0');
+				if (j == 9)
+				{
+					continue;
+				}
+				_putchar(',');
+				_putchar(' ');
+				continue;
+			}
+			_putchar(mul + '0');
+			if (j == 9)
+			{
+				continue;
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+		putchar('\n');
+	}
+}
