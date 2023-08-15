832317c0e15387012133a706bc3927f901227256
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-15 15:20:32 +0100

    40 lines

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,58 +2,38 @@
 
 /**
  * print_times_table - Function
- * Description: return the sum of 2 int
+ * Description: prints n times table with reversed digits for 2+ digit numbers
  * @n: parameter
  */
 void print_times_table(int n)
 {
-	int i, j;
-
 	if (n > 15 || n < 0)
 		return;
 
-	for (i = 0; i <= n; i++)
+	for (int i = 0; i <= n; i++)
 	{
-		for (j = 0; j <= n; j++)
+		for (int j = 0; j <= n; j++)
 		{
 			int mul = i * j;
-			int k;
+			int rev = 0, count = 0;
 
 			if (j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+				putchar(','), putchar(' ');
 			if (mul < 10 && j != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-			}
+				putchar(' '), putchar(' ');
 			else if (mul < 100 && j != 0)
-				_putchar(' ');
+				putchar(' ');
+
 			if (mul > 9)
 			{
-				int count = 0;
-				int rev = 0;
-
 				while (mul != 0)
-				{
-					rev = rev * 10 + mul % 10;
-					mul /= 10;
-					count++;
-				}
-
-				for (k = 0; k < count; k++)
-				{
-					_putchar(rev % 10 + '0');
-					rev /= 10;
-				}
+					rev = rev * 10 + mul % 10, mul /= 10, count++;
+				for (int k = 0; k < count; k++)
+					putchar(rev % 10 + '0'), rev /= 10;
 			}
 			else
-			{
-				_putchar(mul + '0');
-			}
+				putchar(mul + '0');
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
