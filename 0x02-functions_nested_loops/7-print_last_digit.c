fc0aded9d93894b781ad4986b2723f0ff336d47f
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-15 12:58:11 +0100

    print the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - Function
+ * @c: character to check
+ * Description: checks if c is negative and make it positive
+ * Return: c if positive
+ */
+int print_last_digit(int c)
+{
+	int last;
+
+	last = c % 10;
+	_putchar(last);
+	return (last);
+}
