63177d1162066a06a8600fd7c51b32ef926f2f80
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-15 12:37:48 +0100

    last commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Function
+ * Description: prints the alpha ten times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char A;
+
+	for (i = 0; i < 10; i++)
+	{
+		while (A != 123)
+		{
+			_putchar(A);
+			A++;
+		}
+		_putchar('\n');
+	}
+}
