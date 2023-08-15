1d9dff38dc17170b2e7aad5c72048611eafbeb81
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-15 12:57:10 +0100

    print the last digit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,7 +10,7 @@ int _abs(int c);
 int print_sign(int n);
 int _isalpha(int c);
 int _islower(int c);
-int print_last_digit(int);
+int print_last_digit(int c);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
