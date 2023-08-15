9a8832c3633f27fde5173a86d622a61e77b8c8ca
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-15 13:21:49 +0100

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_last_digit - Function
+ * @b: character to check
+ * Description: checks if c is negative and make it positive
+ * Return: c if positive
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
