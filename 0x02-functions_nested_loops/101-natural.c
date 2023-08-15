25a3da8a65c513f5df4112652dfb3fe9266da8e9
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-15 15:43:37 +0100

    just testing

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,20 +6,17 @@
  */
 int main(void)
 {
-	int i = 1, sum1 = 0, sum2 = 0, sum = 0;
+	int i, sum;
 
-	while (3 * i < 1024)
+	i = sum = 0;
+	while (i < 1024)
 	{
-		sum1 += 3 * i;
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
 		i++;
 	}
-	i = 1;
-	while (5 * i < 1024)
-	{
-		sum2 += 5 * i;
-		i++;
-	}
-	sum = sum1 + sum2;
 	printf("%d\n", sum);
-	return 0;
+	return (0);
 }
