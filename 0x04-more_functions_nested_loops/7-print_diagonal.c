573aeefb233bd9a53ef3db7b30e943830acf91f8
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-17 14:48:16 +0100

    confidence

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print numbers
+ * @n: parameter
+ */
+void print_diagonal(int n)
+{
+	int i, j, x;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i > 0)
+		{
+			x = i;
+			while (x = !0)
+			{
+				_putchar(' ');
+				x--;
+			}
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
