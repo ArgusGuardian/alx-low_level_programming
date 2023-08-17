d6f66e40b1a5a3d70695a8f1834afc7d9b068e5a
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-17 16:07:50 +0100

    highest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print the highest prime factor
+ * Return: always 0
+ */
+int main(void)
+{
+	unsigned long int i;
+	unsigned long n = 612852475143;
+
+	for (i = 3; i < sqrt(n); i += 2)
+	{
+		while ((n % i == 0) && (n != i))
+		{
+			n = n / i;
+		}
+	}
+	printf("%lu\n", i);
+	return (0);
+}
