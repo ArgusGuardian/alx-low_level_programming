0fafa79e92cef80947ca749ef3dd42ed371382e9
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-17 13:36:58 +0100

    the product of 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - returns the largest of 3 numbers
+ * @a: parameter
+ * @b: parameter
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
