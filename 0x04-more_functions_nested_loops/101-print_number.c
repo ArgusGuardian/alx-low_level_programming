fa0e9ca549559a67e2718246a2312d09176bb221
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-17 17:12:24 +0100

    last commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,33 +6,15 @@
  */
 void print_number(int n)
 {
-	int x = 0, alert;
+	unsigned int z = n;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
 	if (n < 0)
 	{
-		n = -n;
-		alert = 1;
+		_putchar('-');
+		z = -z;
 	}
+	if (z / 10)
+		print_number(z / 10);
 
-	while (n != 0)
-	{
-		x = (x * 10) + n % 10;
-		n /= 10;
-	}
-
-	while (x != 0)
-	{
-		if (alert == 1)
-		{
-			_putchar('-');
-			alert = 0;
-		}
-		_putchar(x % 10 + '0');
-		x /= 10;
-	}
+	_putchar(z % 10 + '0');
 }
