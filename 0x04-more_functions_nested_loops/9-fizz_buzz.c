a125755b49c9cb5660742997ee659bae3aaa1874
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-17 15:08:49 +0100

    no brackets

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - print numbers from 1 to 100
+ * Return: always 0;
+ */
+int main(void)
+{
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 5 == 0 && i % 3 == 0)
+			printf("FizzBuzz");
+
+		else if (i % 5 == 0)
+			printf("Buzz");
+
+		else if (i % 3 == 0)
+			printf("Fizz");
+
+		else
+			printf("%d", i);
+	}
+	return 0;
+}
