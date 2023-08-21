a19a093856aa9ae32699a97c349ebf279c6cebc4
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-21 10:30:46 +0100

    return the lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - function to swap 2 integers
+ * @s: pointer to a string
+ * Return: returns the lenght of a string
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (s != NULL)
+		count++;
+
+	return (count);
+}
