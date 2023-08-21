fe02aa90b006df83ddeb7e888b0fb1bae325ce95
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-21 14:51:47 +0100

    this is  my code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+int main(void)
+{
+	int sum = 0;
+	int rd = 0;
+	time_t t;
+
+	srand((unsigned int)time(&t));
+	while (sum < 2772)
+	{
+		rd = rand() % 128;
+
+		if ((sum + rd) > 2772)
+			break;
+		sum += rd;
+		printf("%c", rd);
+	}
+	printf("%c\n", (2772 - sum));
+
+	return 0;
+}
