9ec6894649f690aa251a08d56e26d52986e64c0e
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-21 11:27:54 +0100

    removed i

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /**
- * puts2 - print every other character
+ * puts2 - print every other
  * @str: pointer to a string
  */
 void puts2(char *str)
 {
-	int i = 0;
-
 	while (*str != '\0')
 	{
 		_putchar(*str);
