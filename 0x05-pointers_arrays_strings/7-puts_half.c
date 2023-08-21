2b309e0cc3eb031847413b09105e24e4623ec9df
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-21 13:28:52 +0100

    hard code, maybe?

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,30 +6,31 @@
 void puts_half(char *str)
 {
 	int len = 0, i = 0, n;
-
 	while (str[i] != '\0')
 	{
 		len++;
 		i++;
 	}
+	len--;
 
 	if (len % 2 != 0)
 	{
 		n = (len - 1) / 2;
 		while (str[n] != '\0')
 		{
-			_putchar(str[n]);
+			putchar(str[n]);
 			n++;
 		}
 	}
 	else
 	{
+		len += 2;
 		i = len / 2;
 		while (str[i] != '\0')
 		{
-			_putchar(str[i]);
+			putchar(str[i]);
 			i++;
 		}
 	}
-	_putchar('\n');
+	putchar('\n');
 }
