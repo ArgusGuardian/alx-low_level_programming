0abcd1af9057793f674e6670cc2bfedc11be8555
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-23 15:45:34 +0100

    the second task done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,20 +1,18 @@
 #include "main.h"
 /**
- * _strcat - ammend string to another string
+ * _strncat - ammend string to another string
  * @src: pointer to string
  * @dest: pointer to string
+ * @n: number of characters to ammend
  * Return: pointer to string
  */
-char *_strcat(char *dest, char *src)
+void reverse_array(int *a, int n)
 {
-	int i = 0, j = 0;
+	int i = 0, j;
 
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0')
+	for (j = 0; j < n && j != '\0'; j++)
 	{
 		dest[i] = src[j];
-		j++;
 		i++;
 	}
 	dest[i] = '\0';
