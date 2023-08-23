ef7e3055887a1d7901c496c4b202c5b5fb53264a
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-23 18:28:18 +0100

    first try

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * infinite_add - adds big numbers
+ * @n1: string
+ * @n2: string
+ * @r: string
+ * @size_r: size of r string
+ * Return: 0 if k > r, pointer to r if not
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i = 0, j = 0, k = 0, sum, change = 0, l;
+	char tmp;
+
+	while (n1[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (n2[j] != '\0')
+	{
+		j++;
+	}
+	j--;
+
+	while (n1[i] != '\0' || n2[j] != '\0')
+	{
+		sum = (n1[i] - '0') + (n2[i] - '0') + change;
+		if (sum >= 10)
+		{
+			sum = sum % 10;
+			change = 1;
+		}
+		r[k] = sum + '0';
+
+		k++;
+		i--;
+		j--;
+	}
+	if (k > size_r)
+		return (0);
+
+	for (l = 0; l < k / 2; l++, k--)
+	{
+		tmp = r[l];
+		r[l] = r[k];
+		r[k] = tmp;
+	}
+	return (r);
+}
