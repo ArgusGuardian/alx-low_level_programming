9fc263743a801e83e37002ffbca926748e74c7b8
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-23 19:25:04 +0100

    changed things

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,52 +9,35 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, k = 0, sum, change = 0, l, digit1, digit2;
+	int i, j, k, sum, carry = 0, l, digit1, digit2;
 	char tmp;
 
-	while (n1[i] != '\0')
-		i++;
-	i--;
-	while (n2[j] != '\0')
-		j++;
-	j--;
-
-	while (i >= 0 || j >= 0)
-	{
-		if (i >= 0)
-		{
-			digit1 = (n1[i] - '0');
-		}
-		if (j >= 0)
-		{
-			digit2 = (n2[j] - '0');
-		}
-		sum = digit1 + digit2 + change;
-		if (i < 0 && j < 0 && sum == 0)
-		{
-			break;
-		}
-		change = sum / 10;
-		r[k] = (sum % 10) + '0';
-		k++;
-		i--;
-		j--;
-	}
-	if (change)
-	{
-		r[k] = change + '0';
-		k++;
-	}
-	r[k] = '\0';
-	if (k >= size_r)
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
 		return (0);
 
-	k--;
-	for (l = 0; l < k / 2; l++, k--)
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
-		tmp = r[l];
-		r[l] = r[k];
-		r[k] = tmp;
+		digit1 = (i >= 0) ? (n1[i] - '0') : 0;
+		digit2 = (j >= 0) ? (n2[j] - '0') : 0;
+
+		sum = digit1 + digit2 + carry;
+		carry = sum / 10;
+		r[k] = (sum % 10) + '0';
+	}
+	r[k] = '\0';
+
+	if (i >= 0 || j >= 0 || carry)
+		return (0);
+
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		tmp = r[k];
+		r[k] = r[l];
+		r[l] = tmp;
 	}
 
 	return (r);
