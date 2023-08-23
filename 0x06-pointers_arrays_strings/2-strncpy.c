ca5083fc5ec70650c0db2c84fa920ec24961a7f6
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-23 18:43:20 +0100

    added terminators

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,16 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0, j;
 
-	for (j = 0; j < n && j != '\0'; j++)
+	for (i = 0; i < n; i++)
 	{
-		dest[i] = src[j];
+		if (src[i] == 0)
+			break;
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
 		i++;
 	}
-	dest[i] = '\0';
 	return (dest);
 }
