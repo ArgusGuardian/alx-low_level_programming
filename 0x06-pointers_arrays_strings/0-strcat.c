05354721549c1addfd957d06341ecff2f431f098
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-23 15:34:17 +0100

    first code

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,3 +4,19 @@
  * @s: pointer to string
  * Return: integer
  */
+char *_strcat(char *dest, char *src)
+{
+	char *tmp;
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return dest;
+}
