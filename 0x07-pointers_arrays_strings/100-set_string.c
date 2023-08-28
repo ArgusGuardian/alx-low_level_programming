7cf72f3651d530f2cd39b8f7263f21f0a0ac95ca
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-28 16:44:36 +0100

    fantastic

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - function prints sum of 2 diagonals
+ * @s: pointer to double string
+ * @to: pointer to string
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
