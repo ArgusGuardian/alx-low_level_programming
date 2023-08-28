c8cb35e16645313cf36701af3f329fa43e9f5d10
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-28 16:38:58 +0100

    start from size-1

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,21 +2,15 @@
 /**
  * print_diagsums - function prints sum of 2 diagonals
  * @a: pointer to double array
+ * @size: size of the double arrays
  */
 void print_diagsums(int *a, int size)
 {
 	int i, j, sum1 = 0, sum2 = 0;
-	int diagonal1 = 0, diagonal2 = 0;
 
-	for (i = 0; i < size * size && diagonal1 <= size; i += size + 1)
-	{
+	for (i = 0; i < size * size; i += size + 1)
 		sum1 += a[i];
-		diagonal1++;
-	}
-	for (j = 0; j < size * size && diagonal2 <= size; j += size - 1)
-	{
+	for (j = size - 1; j < (size * size) - size; j += size - 1)
 		sum2 += a[j];
-		diagonal2++;
-	}
 	printf("%d, %d\n", sum1, sum2);
 }
