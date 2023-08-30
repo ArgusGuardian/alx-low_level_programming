26789f431c6f67d19d1c0fc101087cb1d2b96608
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-30 10:49:04 +0100

    second task

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print string in reverse
+ *@s: pointer to string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_puts_recursion(s + 1);
+	putchar(*s);
+}
