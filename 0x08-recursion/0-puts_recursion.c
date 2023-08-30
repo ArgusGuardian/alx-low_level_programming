79d33dfb05fedfba263bbc33d0c8f255f417e17f
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-08-30 10:38:55 +0100

    first commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,7 @@
 #include "main.h"
 /**
- * _memset - function that fills n bytes
+ * _puts_recursion - print string in recursion
  *@s: pointer to memory
- *@b: character
- *@n: number of bytes to be filled
- * Return: return pointer to the memory
  */
 void _puts_recursion(char *s)
 {
