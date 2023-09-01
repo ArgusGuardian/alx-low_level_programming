799a584c1f39d31bb82615fb9ca101737c12dd81
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-01 11:28:42 +0100

    edit the isalpha file

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,12 +1,11 @@
-vim #include "main.h"
-	/**
-	 * _isalpha - Function
-	 * @c: character to check
-	 * Description: checks if c is lowercase or not
-	 * Return: 1 if c is lowercase 0 if not
-	 */
-	int
-	_isalpha(int c)
+#include "main.h"
+/**
+ * _isalpha - Function
+ * @c: character to check
+ * Description: checks if c is lowercase or not
+ * Return: 1 if c is lowercase 0 if not
+ */
+int _isalpha(int c)
 {
 	if ((c <= 122 && c >= 97) || (c >= 65 && c <= 90))
 		return (1);
