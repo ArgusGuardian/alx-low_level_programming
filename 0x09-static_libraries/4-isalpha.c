631afcd6a18bea5f9d000d8d96b64269ec964d3c
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-01 11:28:18 +0100

    second push

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,11 +1,12 @@
-#include "main.h"
-/**
- * _isalpha - Function
- * @c: character to check
- * Description: checks if c is lowercase or not
- * Return: 1 if c is lowercase 0 if not
- */
-int _isalpha(int c)
+vim #include "main.h"
+	/**
+	 * _isalpha - Function
+	 * @c: character to check
+	 * Description: checks if c is lowercase or not
+	 * Return: 1 if c is lowercase 0 if not
+	 */
+	int
+	_isalpha(int c)
 {
 	if ((c <= 122 && c >= 97) || (c >= 65 && c <= 90))
 		return (1);
