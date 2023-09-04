72697ea6e239c08b4605d4ae203c2087883916cb
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-04 15:09:58 +0100

    first commmit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * alloc_grid - allocate memory for a grid
+ *@width: width of the grid
+ *@height: height of the grid
+ * Return: pointer to grid
+ *
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i = 0, j = 0;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = (int **)malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		*(grid + i) = (int *)malloc(sizeof(int) * width);
+		if (grid + i == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(*(grid + j));
+			free(grid);
+			return (NULL);
+		}
+	}
+
+	while (i < height * width)
+		**(grid + i) = 0;
+
+	return (grid);
+}
