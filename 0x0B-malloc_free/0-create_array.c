eded633a218085e580886ab94971f618601253cd
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-04 13:47:33 +0100

    first commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * create_array - functinon that creates an array
+ *@size: size of the array
+ *@c: character
+ * Return: pointer to array Null if fails or size = 0
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	char *string = malloc(size + 1);
+
+	if (string == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		string[i] = c;
+	string[i] = '\0';
+
+	return (string);
+}
