42d2e773fa072faab6fce33374f2e7119b8353ec
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-04 14:50:13 +0100

    the order of checking wether Null or not

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,22 +11,11 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, k = 0, len1 = 0, len2 = 0;
 	char *concat;
 
-	if (s1 != NULL)
-	{
-		while (s1[i] != '\0')
-		{
-			len1++;
-			i++;
-		}
-	}
-	if (s2 != NULL)
-	{
-		while (s2[j] != '\0')
-		{
-			len2++;
-			j++;
-		}
-	}
+	while (s1 && s1[len1] != '\0')
+		len1++;
+	while (s2 && s2[len2] != '\0')
+		len2++;
+
 	concat = (char *)malloc(len1 + len2 + 1);
 	if (concat == NULL)
 		return (NULL);
@@ -34,13 +23,13 @@ char *str_concat(char *s1, char *s2)
 	i = j = 0;
 	while (k < len1 + len2)
 	{
-		if (s1[i] != '\0' && s1 != NULL)
+		if (s1 != NULL && s1[i] != '\0')
 		{
 			concat[k] = s1[i];
 			i++;
 			k++;
 		}
-		else if (s2[j] != '\0' && s2 != NULL)
+		else if (s2 != NULL && s2[j] != '\0')
 		{
 			concat[k] = s2[j];
 			j++;
