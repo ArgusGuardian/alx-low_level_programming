4f5de5768748ef6b94aaa66619f54f7c3183047c
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-04 15:17:02 +0100

    test

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		*(grid + i) = (int *)malloc(sizeof(int) * width);
-		if (grid + i == NULL)
+		if (*(grid + i) == NULL)
 		{
 			for (j = 0; j < i; j++)
 				free(*(grid + j));
@@ -37,4 +37,45 @@ int **alloc_grid(int width, int height)
 	}
 
 	return (grid);
+}
+
+void print_grid(int **grid, int width, int height)
+{
+	int w;
+	int h;
+
+	h = 0;
+	while (h < height)
+	{
+		w = 0;
+		while (w < width)
+		{
+			printf("%d ", grid[h][w]);
+			w++;
+		}
+		printf("\n");
+		h++;
+	}
+}
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int **grid;
+
+	grid = alloc_grid(6, 4);
+	if (grid == NULL)
+	{
+		return (1);
+	}
+	print_grid(grid, 6, 4);
+	printf("\n");
+	grid[0][3] = 98;
+	grid[3][4] = 402;
+	print_grid(grid, 6, 4);
+	return (0);
 }
