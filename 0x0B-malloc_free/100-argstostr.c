e34d5ab4b96494ef68b524d22a2513373caa8067
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-05 11:59:14 +0100

    easy no main

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -42,17 +42,3 @@ char *argstostr(int ac, char **av)
 	concat[k] = '\0';
 	return (concat);
 }
-
-int main(int ac, char *av[])
-{
-	char *s;
-
-	s = argstostr(ac, av);
-	if (s == NULL)
-	{
-		return (1);
-	}
-	printf("%s", s);
-	free(s);
-	return (0);
-}
