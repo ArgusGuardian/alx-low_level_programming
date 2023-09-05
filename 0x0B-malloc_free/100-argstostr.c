172104a982cf8a49658c9123ecde0259ed5009f4
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-05 11:58:48 +0100

    easy

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,58 @@
+#include "main.h"
+/**
+ * argstostr - concat all the arguments
+ *@ac: number of arguments
+ *@av: pointer to strings
+ *Return: return poitner to string, NULL if failed
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k = 0, len = 0;
+	char *concat;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			len++;
+			j++;
+		}
+	}
+	len += ac + 1;
+	concat = (char *)malloc(len);
+	if (concat == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			concat[k] = av[i][j];
+			j++;
+			k++;
+		}
+		concat[k] = '\n';
+		k++;
+	}
+	concat[k] = '\0';
+	return (concat);
+}
+
+int main(int ac, char *av[])
+{
+	char *s;
+
+	s = argstostr(ac, av);
+	if (s == NULL)
+	{
+		return (1);
+	}
+	printf("%s", s);
+	free(s);
+	return (0);
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * free_grid - free 2 array
+ *@grid: pointer to grid
+ *@height: height of the grid
+ *
+ */
+char **strtow(char *str);
