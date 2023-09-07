f23fd17a90e40d24586ad1ffaaced571a4f6be22
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-07 16:24:43 +0100

    last hope

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,24 +17,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2 != NULL && s2[len2] != '\0')
 		len2++;
 	if (n >= len2)
-	{
 		concat = (char *)malloc(len1 + len2 + 1), alert = 0;
-		if (concat == NULL)
-			return (NULL);
-	}
 	else
-	{
 		concat = (char *)malloc(len1 + n + 1), alert = 1;
-		if (concat == NULL)
-			return (NULL);
-	}
-	while (s1[i] != '\0')
+
+	if (concat == NULL)
+		return (NULL);
+
+	while (s1 != NULL && s1[i] != '\0')
 		concat[i] = s1[i], i++;
 	if (alert == 0)
-		for (j = 0; s2[j] != '\0'; i++, j++)
+		for (j = 0; s2 != NULL && s2[j] != '\0'; i++, j++)
 			concat[i] = s2[j];
 	else
-		for (j = 0; n > 0; i++, j++, n--)
+		for (j = 0; s2 != NULL && n > 0; i++, j++, n--)
 			concat[i] = s2[j];
 	concat[i] = '\0';
 	return (concat);
