583f6efa45f117285ca5146070ff5ec374188cd0
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-07 16:51:32 +0100

    fourth thask

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * array_range - functinon allocates memory
+ *@min: min number
+ *@max: max number
+ * Return: pointer
+ *
+ */
+int *array_range(int min, int max)
+{
+	int *array;
+	int diff, i;
+
+	diff = min - max + 1;
+	if (diff <= 0)
+		return (NULL);
+
+	array = (int *)malloc(diff * sizeof(int));
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < diff; i++, min++)
+	{
+		array[i] = min;
+	}
+
+	return (array);
+}
