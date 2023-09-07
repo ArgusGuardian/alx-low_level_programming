3f0c50f1ca70104624e473bcfaa06ab5240259f2
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-07 17:25:25 +0100

    last update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,98 +1,89 @@
 #include "main.h"
-
-int is_digit(char c)
+/**
+ * _isNumber - Checks if a string is a number.
+ * @str: Pointer to the string.
+ * Return: 1 if not a number, 0 if a number.
+ **/
+int _isNumber(char *str)
 {
-	return (c >= '0' && c <= '9');
+	for (int i = 0; str[i]; i++)
+		if (str[i] < '0' || str[i] > '9')
+			return 1; // Not a number
+	return 0;		  // It's a number
 }
-
-int str_length(char *str)
+/**
+ * multiplyStrings - Multiplies a string by a single digit.
+ * @num1: The string to be multiplied.
+ * @digit: The single digit to multiply by.
+ * @result: The result string.
+ * @len1: The length of num1.
+ * @lenResult: The length of the result.
+ **/
+void multiplyStrings(char *num1, char digit, char *result, int len1, int lenResult)
 {
-	int length = 0;
-	while (str[length] != '\0')
+	int carry = 0;
+	for (int i = len1 - 1, j = lenResult - 1; i >= 0; i--, j--)
 	{
-		length++;
+		int product = (num1[i] - '0') * (digit - '0') + (result[j] - '0') + carry;
+		carry = product / 10;
+		result[j] = (product % 10) + '0';
 	}
-	return length;
+	while (carry)
+	{
+		int sum = (result[lenResult - 1] - '0') + carry;
+		carry = sum / 10;
+		result[lenResult - 1] = (sum % 10) + '0';
+		lenResult--;
+	}
 }
-
-char *multiply(char *num1, char *num2)
+/**
+ * main - Multiplies two positive numbers.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
+ * Return: 0 if successful, 98 if there's an error.
+ **/
+int main(int argc, char *argv[])
 {
-	int len1 = str_length(num1);
-	int len2 = str_length(num2);
+	int resultLength, len1 = 0, len2 = 0;
+	char *num1 = argv[1];
+	char *num2 = argv[2];
+	char *result;
+	if (argc != 3 || _isNumber(argv[1]) || _isNumber(argv[2]))
+	{
+		fprintf("Error\n");
+		return (98);
+	}
 
-	int result_len = len1 + len2;
-	char *result = calloc(result_len + 1, sizeof(char));
+	while (num1[len1])
+		len1++;
+	while (num2[len2])
+		len2++;
+
+	resultLength = len1 + len2;
+	result = calloc(resultLength + 1, sizeof(char));
 
 	if (result == NULL)
 	{
 		fprintf(stderr, "Error\n");
-		exit(98);
+		return (98);
 	}
 
-	int carry = 0;
-	for (int i = len1 - 1; i >= 0; i--)
-	{
-		int n1 = num1[i] - '0';
+	for (int i = len2 - 1, position = 0; i >= 0; i--, position++)
+		multiplyStrings(num1, num2[i], result, len1, resultLength - position);
 
-		for (int j = len2 - 1; j >= 0; j--)
-		{
-			int n2 = num2[j] - '0';
-
-			int product = (n1 * n2) + (result[i + j + 1] - '0') + carry;
-			carry = product / 10;
-
-			result[i + j + 1] = (product % 10) + '0';
-		}
-	}
-
-	return result;
-}
-
-int main(int argc, char *argv[])
-{
-	if (argc != 3)
-	{
-		fprintf(stderr, "Error\n");
-		return 98;
-	}
-
-	char *num1 = argv[1];
-	char *num2 = argv[2];
-
-	for (int i = 0; num1[i] != '\0'; i++)
-	{
-		if (!is_digit(num1[i]))
-		{
-			fprintf(stderr, "Error\n");
-			return 98;
-		}
-	}
-
-	for (int i = 0; num2[i] != '\0'; i++)
-	{
-		if (!is_digit(num2[i]))
-		{
-			fprintf(stderr, "Error\n");
-			return 98;
-		}
-	}
-
-	char *result = multiply(num1, num2);
-
-	int leading_zeros = 1;
-	for (int i = 0; result[i] != '\0'; i++)
+	int leadingZeros = 1;
+	for (int i = 0; result[i]; i++)
 	{
 		if (result[i] != '0')
 		{
-			leading_zeros = 0;
+			leadingZeros = 0;
 			break;
 		}
-
-		if (leading_zeros)
-			printf("0\n");
-		else
-			printf("%s\n", result);
-		free(result);
-		return 0;
 	}
+	if (leadingZeros)
+		printf("0\n");
+	else
+		printf("%s\n", result);
+	free(result);
+	return (0);
 }
