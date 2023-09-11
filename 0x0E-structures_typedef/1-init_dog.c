d7e39d7d72f8539b3315abf453d62b703a963b2e
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-11 15:53:04 +0100

    first task

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - initialize dog structure
+ * @d: structure type dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->age = age;
+	d->name = name;
+	d->owner = owner;
+}
