56d2e69babf8e60b16bbed1a9fc41fab227474cb
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-13 15:58:31 +0100

    function task

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+/**
+ * print_name - pass name to other functions
+ * @name: name to print
+ * @f: pointer to function
+ */
+int main(int argc, char *argv[])
+{
+	int x, y;
+	int (*op_ptr)(int, int);
+	char op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[3]);
+	op_ptr = get_op_func(argv[2]);
+	if (op_ptr == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	op = *argv[2];
+	if ((op == '/' || op == '%') && y == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", func(x, y));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - adds 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtract 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - product of 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division of 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulo of 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
