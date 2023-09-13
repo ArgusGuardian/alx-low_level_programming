cad015a5ee4a5b1bb6197400b691a5f20da54758
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-13 15:04:40 +0100

    first commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_name - pass name to other functions
+ * @name: name to print
+ * @f: pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
