fa8318eff006c7b5bd68c200185a832559615c11
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-13 15:20:10 +0100

    search function

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - function the manages search for an int
+ * @array: int array
+ * @size: size of the array
+ * @cmp: function pointer
+ * Return: index of the element matching or -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+		else
+			return (-1);
+	}
+}
