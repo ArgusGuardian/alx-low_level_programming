984e0aa1358090cbf777ffd7541b691b1864156d
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-14 18:11:06 +0100

    last commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,34 +12,35 @@ void print_all(const char *const format, ...)
 
 	va_start(args, format);
 
-	if (!format)
-		return;
-	while (format[i])
+	if (format)
 	{
-		switch (format[i])
+		while (format[i])
 		{
-		case 'c':
-			printf("%s%c", sep, va_arg(args, int));
-			break;
-		case 'i':
-			printf("%s%d", sep, va_arg(args, int));
-			break;
-		case 'f':
-			printf("%s%f", sep, va_arg(args, double));
-			break;
-		case 's':
-			word = va_arg(args, char *);
-			if (word == NULL)
-				printf("%s(nil)", sep);
-			else
-				printf("%s%s", sep, word);
-			break;
-		default:
+			switch (format[i])
+			{
+			case 'c':
+				printf("%s%c", sep, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(args, double));
+				break;
+			case 's':
+				word = va_arg(args, char *);
+				if (word == NULL)
+					printf("%s(nil)", sep);
+				else
+					printf("%s%s", sep, word);
+				break;
+			default:
+				i++;
+				continue;
+			}
+			sep = ", ";
 			i++;
-			continue;
 		}
-		sep = ", ";
-		i++;
 	}
 	printf("\n");
 	va_end(args);
