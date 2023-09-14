37e7f85bd7fdef3ffb883bf483f1a7cdb3a47172
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-14 17:10:45 +0100

    stupid

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,4 @@
-#include <variadic_functions.h>
+#include "variadic_functions.h"
 /**
  *sum_them_all - sums all arguments
  *@n: number of arguments
@@ -23,4 +23,14 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_end(args);
 	return (sum);
+}
+int main(void)
+{
+	int sum;
+
+	sum = sum_them_all(2, 98, 1024);
+	printf("%d\n", sum);
+	sum = sum_them_all(4, 98, 1024, 402, -1024);
+	printf("%d\n", sum);
+	return (0);
 }
