481ef863c5b7c3a221849fad733d864ceaaaa15a
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-14 18:01:13 +0100

    update betty

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,8 +1,7 @@
 #include "variadic_functions.h"
 /**
  *print_all - print all the arguments
- *@n: number of arguments
- *@separator: string that seperates the arguments
+ *@format: string
  */
 void print_all(const char *const format, ...)
 {
@@ -19,22 +18,18 @@ void print_all(const char *const format, ...)
 		{
 		case 'c':
 			alert += 1;
-
 			printf("%s%c", sep, va_arg(args, int));
 			break;
 		case 'i':
 			alert += 1;
-
 			printf("%s%d", sep, va_arg(args, int));
 			break;
 		case 'f':
 			alert += 1;
-
 			printf("%s%f", sep, va_arg(args, double));
 			break;
 		case 's':
 			alert += 1;
-
 			word = va_arg(args, char *);
 			if (word == NULL)
 				printf("%s(nil)", sep);
@@ -44,7 +39,6 @@ void print_all(const char *const format, ...)
 		default:
 			i++;
 			continue;
-			break;
 		}
 		sep = ", ";
 		i++;
