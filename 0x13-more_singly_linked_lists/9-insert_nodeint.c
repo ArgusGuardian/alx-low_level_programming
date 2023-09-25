ae927b6e00b1160e1905a7b606483e95878c0ea4
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-25 18:29:53 +0100

    removed unused var

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int count = 0, i;
+	unsigned int i;
 	listint_t *newnode, *fast = *head, *slow = NULL;
 
 	newnode = (listint_t *)malloc(sizeof(listint_t));
