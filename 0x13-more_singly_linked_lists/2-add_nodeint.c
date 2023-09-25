117ecc77c16f9a44d00c79b54a5a5bfa5966e316
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-25 14:50:31 +0100

    first commit

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ *listint_len - return the number of nodes in linked list
+ *@h: linked list
+ *Return: number of nodes
+ *
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newnode;
+
+	newnode = (listint_t *)calloc(sizeof(listint_t));
+}
