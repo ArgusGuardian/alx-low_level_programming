d69c52ef6a2ed1c4b5d3e65224632d90570f47ce
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-25 17:22:21 +0100

    8 task

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,3 +5,21 @@
  *Return: value of the node popped
  *
  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0, i;
+	listint_t *current = head;
+
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+	if (index >= count - 1)
+		return (NULL);
+	current = head;
+	for (i = 0; i < index; i++)
+		current = current->next;
+
+	return (current);
+}
