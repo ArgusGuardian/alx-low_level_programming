cbb151dd1a58df216ef7bf644cf8a373d9203393
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-25 17:45:36 +0100

    ninth task

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+/**
+ *insert_nodeint_at_index - get the sum of all the data in linked list
+ *@head: head of linked list
+ *@idx: index that we should add node at
+ *@n: data for the node
+ *Return: return pointer to the new node
+ *
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int count = 0, i;
+	listint_t *newnode, *fast = *head, *slow = NULL;
+
+	while (fast != NULL)
+	{
+		count++;
+		fast = fast->next;
+	}
+	if (idx > count)
+		return (NULL);
+
+	newnode = (listint_t *)malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->n = n;
+	fast = *head;
+
+	if (idx == 0)
+	{
+		newnode->next = *head;
+		(*head)->next = newnode;
+	}
+	else
+	{
+		for (i = 0; i < idx; i++)
+		{
+			slow = fast;
+			fast = fast->next;
+		}
+		slow->next = newnode;
+		newnode->next = fast;
+	}
+	return (newnode);
+}
