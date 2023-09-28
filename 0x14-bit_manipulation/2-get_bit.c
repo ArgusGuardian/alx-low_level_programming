20bcca972bfe41a39cfaab199a98a03a01fd0e8c
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-28 19:31:43 +0100

    third push

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *get_bit - return the bit pointed to by the index
+ *@n: decimal
+ *@index: index of the bit to return
+ *Return: 0 or 1
+ *
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > sizeof(unsigned long int))
+		return (-1);
+	if (n & (1 << index))
+		return (1);
+	else
+		return (0);
+}
