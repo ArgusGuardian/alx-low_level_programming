71a8ed9c77ab4ff94e9ab3b727a6d24a43e52208
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-09-28 19:46:16 +0100

    4th push

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,14 +3,14 @@
  *get_bit - return the bit pointed to by the index
  *@n: decimal
  *@index: index of the bit to return
- *Return: 0 or 1
+ *Return: -1 or 1 or 0
  *
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > sizeof(unsigned long int))
+	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
-	if (1 & (n >> index))
+	if (n & (1 << index))
 		return (1);
 	else
 		return (0);
