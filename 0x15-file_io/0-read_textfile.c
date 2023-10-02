646680d41ec2f93222f226ae6ad1241aa03fc76b
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-10-02 13:43:18 +0100

    update first task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,8 +7,9 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buff;
-	ssize_t fd, rd, wd;
+	int fd;
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
@@ -17,28 +18,31 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	buff = malloc(letters + 1);
-	if (buff == NULL)
+	buffer = malloc(letters);
+	if (buffer == NULL)
 	{
-		close(fd);
-		return (0);
-	}
-	rd = read(fd, buff, letters);
-	if (rd == -1)
-	{
-		free(buff);
-		close(fd);
-		return (0);
-	}
-	wd = write(STDOUT_FILENO, buff, letters);
-	if (wd == -1 || wd != rd)
-	{
-		free(buff);
 		close(fd);
 		return (0);
 	}
 
-	free(buff);
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || (size_t)bytes_written != letters)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
 	close(fd);
-	return (wd);
+
+	return (bytes_written);
 }
