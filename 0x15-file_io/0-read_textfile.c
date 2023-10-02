838eff867b8a4cf0b6282a46b136b782792cc147
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-10-02 11:17:14 +0100

    first push

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * read_textfile - read from file and print to standard output
+ * @filename: targeted filename
+ * @letters: number of letter to be read and printed
+ * Return: number of chars printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char buff[letters];
+	int fd, rd, wd;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	rd = read(fd, buff, letters);
+	if (rd == -1)
+		return (0);
+	wd = write(STDOUT_FILENO, buff, letters);
+	if (wd == -1)
+		return (0);
+
+	return (letters);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,2 @@
+#include "main.h"
+int append_text_to_file(const char *filename, char *text_content);
