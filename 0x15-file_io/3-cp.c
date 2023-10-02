d2131e8b510fe106fc680756e009224be32d2446
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-10-02 13:22:21 +0100

    push fourth task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,1 +1,74 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+/**
+ * error_exit - exit helper function
+ * @code: code to exit with
+ * @message: message to print
+ * @arg: argument
+ */
+
+void error_exit(int code, const char *message, const char *arg)
+{
+	dprintf(2, message, arg);
+	exit(code);
+}
+/**
+ * error_exit2 - exit helper function
+ * @code: code to exit with
+ * @message: message to print
+ * @arg: argument
+ */
+
+void error_exit2(int code, const char *message, int arg)
+{
+	dprintf(2, message, arg);
+	exit(code);
+}
+/**
+ * main - read from file_from and copy to file_to
+ * @argc: number of terminal args
+ * @argv: terminal args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, len = 0;
+	ssize_t rd, wd;
+	char *buff;
+
+	if (argc != 3)
+		error_exit(97, "Usage: cp file_from file_to\n", "");
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+		error_exit(99, "Error: Can't write to file %s\n", argv[2]);
+
+	while ((*argv)[len])
+		len++;
+	buff = (char *)malloc(len);
+	if (buff == NULL)
+		error_exit(99, "Error: Can't write to file %s\n", argv[2]);
+
+	while ((rd = read(fd_from, buff, len)) > 0)
+	{
+		wd = write(fd_to, buff, rd);
+		if (wd == -1)
+			free(buff), error_exit(99, "Error: Can't write to file %s\n", argv[2]);
+	}
+	if (rd == -1)
+		free(buff), error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+
+	if (close(fd_from) == -1)
+		free(buff), error_exit2(100, "Error: Can't close fd %d\n", fd_from);
+
+	if (close(fd_to) == -1)
+		free(buff), error_exit2(100, "Error: Can't close fd %d\n", fd_to);
+
+	free(buff);
+	return (0);
+}
