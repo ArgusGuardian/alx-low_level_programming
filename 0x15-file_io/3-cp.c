a4361bd4c6f2f300dd797ab15deb322a52c1e53a
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-10-02 13:29:02 +0100

    closed files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -43,32 +43,35 @@ int main(int argc, char *argv[])
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
+	{
+		close(fd_from);
 		error_exit(99, "Error: Can't write to file %s\n", argv[2]);
-
+	}
 	while ((*argv)[len])
 		len++;
 	buff = (char *)malloc(len);
 	if (buff == NULL)
 		error_exit(99, "Error: Can't write to file %s\n", argv[2]);
-
 	while ((rd = read(fd_from, buff, len)) > 0)
 	{
 		wd = write(fd_to, buff, rd);
 		if (wd == -1)
+		{
+			close(fd_from), close(fd_to);
 			free(buff), error_exit(99, "Error: Can't write to file %s\n", argv[2]);
+		}
 	}
 	if (rd == -1)
+	{
+		close(fd_from), close(fd_to);
 		free(buff), error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-
+	}
 	if (close(fd_from) == -1)
 		free(buff), error_exit2(100, "Error: Can't close fd %d\n", fd_from);
-
 	if (close(fd_to) == -1)
 		free(buff), error_exit2(100, "Error: Can't close fd %d\n", fd_to);
-
 	free(buff);
 	return (0);
 }
