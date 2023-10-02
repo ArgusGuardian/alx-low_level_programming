aaada627e5b106353df39ce2b588d2f27a649128
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-10-02 12:29:54 +0100

    third task

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,2 +1,27 @@
 #include "main.h"
-int append_text_to_file(const char *filename, char *text_content);
+/**
+ * append_text_to_file - read from file and print to standard output
+ * @filename: targeted file name
+ * @text_content: content
+ * Return: 1 or -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, wd, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_APPEND | O_RDWR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+	while (text_content[len])
+		len++;
+
+	wd = write(fd, text_content, len);
+	if (wd == -1)
+		return (-1);
+	return (1);
+}
