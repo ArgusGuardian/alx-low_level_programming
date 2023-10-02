fe8418418cdf081398d9e0f92cf2d5c2bb334045
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-10-02 13:52:00 +0100

    change code completly

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,40 +7,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t rd, wd, fd;
-	char *buff;
+	ssize_t fd, rd, wd;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+	rd = read(fd, buffer, letters);
+	wd = write(STDOUT_FILENO, buffer, rd);
 
-	buff = malloc(letters);
-	if (buff == NULL)
+	if (fd == -1 || rd == -1 || wd == -1 || wd != rd)
 	{
+		free(buffer);
 		close(fd);
 		return (0);
 	}
 
-	rd = read(fd, buff, letters);
-	if (rd == -1)
-	{
-		free(buff);
-		close(fd);
-		return (0);
-	}
-
-	wd = write(STDOUT_FILENO, buff, rd);
-	if (wd == -1 || (size_t)wd != letters)
-	{
-		free(buff);
-		close(fd);
-		return (0);
-	}
-
-	free(buff);
+	free(buffer);
 	close(fd);
 
 	return (wd);
