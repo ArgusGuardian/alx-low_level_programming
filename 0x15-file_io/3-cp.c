fa725fcd6897dfbcf60f44ba28693838b47909e8
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-10-02 13:37:39 +0100

    no dynamic allocation

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 {
 	int fd_from, fd_to, len = 0;
 	ssize_t rd, wd;
-	char *buff;
+	char *buff[1024];
 
 	if (argc != 3)
 		error_exit(97, "Usage: cp file_from file_to\n", "");
@@ -39,7 +39,6 @@ int main(int argc, char *argv[])
 	}
 	while ((*argv)[len])
 		len++;
-	buff = (char *)malloc(len);
 	if (buff == NULL)
 		error_exit(99, "Error: Can't write to file %s\n", argv[2]);
 	while ((rd = read(fd_from, buff, len)) > 0)
@@ -48,16 +47,15 @@ int main(int argc, char *argv[])
 		if (wd == -1)
 		{
 			close(fd_from), close(fd_to);
-			free(buff), error_exit(99, "Error: Can't write to file %s\n", argv[2]);
+			error_exit(99, "Error: Can't write to file %s\n", argv[2]);
 		}
 	}
 	if (rd == -1)
 	{
 		close(fd_from), close(fd_to);
-		free(buff), error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 	}
 	close(fd_from);
 	close(fd_to);
-	free(buff);
 	return (0);
 }
