e19e8094424bae7f0058c58fb376bcf640267a47
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-10-02 11:25:52 +0100

    second commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,7 +7,7 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char buff[letters];
+	char *buff;
 	int fd, rd, wd;
 
 	if (filename == NULL)
@@ -15,14 +15,58 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		close(fd);
 		return (0);
+	}
 
+	buff = malloc(letters);
+	if (buff == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 	rd = read(fd, buff, letters);
 	if (rd == -1)
+	{
+		free(buff);
+		close(fd);
 		return (0);
+	}
 	wd = write(STDOUT_FILENO, buff, letters);
 	if (wd == -1)
+	{
+		free(buff);
+		close(fd);
 		return (0);
+	}
 
-	return (letters);
+	free(buff);
+	close(fd);
+	return (wd);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	ssize_t n;
+
+	if (ac != 2)
+	{
+		dprintf(2, "Usage: %s filename\n", av[0]);
+		exit(1);
+	}
+	n = read_textfile(av[1], 114);
+	printf("\n(printed chars: %li)\n", n);
+	n = read_textfile(av[1], 1024);
+	printf("\n(printed chars: %li)\n", n);
+	return (0);
 }
