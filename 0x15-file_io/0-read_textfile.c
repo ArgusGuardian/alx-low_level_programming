626b6fd4c5dd4af3486a1f4f0dbc7cf78cbf85fe
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-10-02 11:28:45 +0100

    no close int the first condition + size_t

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,17 +8,14 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buff;
-	int fd, rd, wd;
+	size_t fd, rd, wd;
 
 	if (filename == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
-		close(fd);
 		return (0);
-	}
 
 	buff = malloc(letters);
 	if (buff == NULL)
