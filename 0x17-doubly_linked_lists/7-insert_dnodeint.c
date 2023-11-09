ac22528aba4e4967dd93d51f4bdd98b1a51abbb1
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-09 18:22:55 +0100

    8 tasl done

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - sum of all data in linked list
+ *@h: header
+ *@idx: index to insert to
+ * @n: data to add
+ * Return: the node added
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new;
+	dlistint_t *fast = *h;
+	dlistint_t *slow = NULL;
+	unsigned int counter;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+
+	if (!(*h))
+	{
+		(*h) = new;
+		new->prev = NULL;
+		new->next = NULL;
+	}
+	else
+	{
+		for (counter = 0; counter < idx && fast != NULL; counter++)
+		{
+			slow = fast;
+			fast = fast->next;
+		}
+		new->prev = slow;
+		if (slow)
+			slow->next = new;
+		new->next = fast;
+		if (fast)
+			fast->prev = new;
+		if (idx == 0)
+			*h = new;
+	}
+	return (new);
+}
