5ba142d31508d80a6d69fb8d8be39bcb3f69e7be
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-09 19:15:57 +0100

    another go

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,46 +1,45 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
 /**
- * magicKeyGenerator - cook up a secret sauce based on a username for my awesome app
- * @argc_boi: the number of things you threw at this program
- * @argv_boi: the things you threw, like a username
+ * main - generate a pass depending on username for the app
+ * @argc: number of arguments passed
+ * @argv: arguments passed to main
  *
  * Return: 0 if it worked, 1 if something went sideways
  */
-int main(int argc_boi, char *argv_boi[])
+int main(int argc, char *argv[])
 {
 	unsigned int j, a;
-	size_t len, add;
+	size_t lenght, sum;
 	char *sauce;
 	char recipe[7] = "      ";
 
 	sauce = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-	if (argc_boi != 2)
+	if (argc != 2)
 	{
-		printf("Bruh, use it right: ./secretgen username\n");
+		printf("Correct usage: ./keygen5 username\n");
 		return (1);
 	}
-	len = strlen(argv_boi[1]);
-	recipe[0] = sauce[(len ^ 59) & 63];
-	for (j = 0, add = 0; j < len; j++)
-		add += argv_boi[1][j];
-	recipe[1] = sauce[(add ^ 79) & 63];
-	for (j = 0, a = 1; j < len; j++)
-		a *= argv_boi[1][j];
+	lenght = strlen(argv[1]);
+	recipe[0] = sauce[(lenght ^ 59) & 63];
+	for (j = 0, sum = 0; j < lenght; j++)
+		sum += argv[1][j];
+	recipe[1] = sauce[(sum ^ 79) & 63];
+	for (j = 0, a = 1; j < lenght; j++)
+		a *= argv[1][j];
 	recipe[2] = sauce[(a ^ 85) & 63];
-	for (a = argv_boi[1][0], j = 0; j < len; j++)
-		if ((char)a <= argv_boi[1][j])
-			a = argv_boi[1][j];
+	for (a = argv[1][0], j = 0; j < lenght; j++)
+		if ((char)a <= argv[1][j])
+			a = argv[1][j];
 	srand(a ^ 14);
 	recipe[3] = sauce[rand() & 63];
-	for (a = 0, j = 0; j < len; j++)
-		a += argv_boi[1][j] * argv_boi[1][j];
+	for (a = 0, j = 0; j < lenght; j++)
+		a += argv[1][j] * argv[1][j];
 	recipe[4] = sauce[(a ^ 239) & 63];
-	for (a = 0, j = 0; (char)j < argv_boi[1][0]; j++)
+	for (a = 0, j = 0; (char)j < argv[1][0]; j++)
 		a = rand();
 	recipe[5] = sauce[(a ^ 229) & 63];
-	printf("Voila! Your secret sauce is: %s\n", recipe);
+	printf("%s\n", recipe);
 	return (0);
 }
