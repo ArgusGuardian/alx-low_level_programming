a9536a65184d6a626b498b640bf5dbd047034bdf
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-11-09 16:18:25 +0100

    betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * print_dlistint - print list
- *
+ *@h: header
  * Return: returns the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
@@ -14,5 +14,5 @@ size_t print_dlistint(const dlistint_t *h)
 		h = h->next;
 		counter++;
 	}
-	return counter;
+	return (counter);
 }
