222457beeeb3ae65b1449711485691d6c68c9cc8
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-01 03:48:13 +0100

    betty done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,12 +11,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
-		return NULL;
+		return (NULL);
 
 	table->size = size;
 	table->array = malloc(size * sizeof(hash_node_t *));
 	if (!table->array)
-		return NULL;
+		return ();
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 
