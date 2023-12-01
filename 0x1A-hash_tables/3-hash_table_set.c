a74b6dbede99e664cecffff88985cfef178731e7
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-01 04:08:10 +0100

    betty done

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,55 +1,70 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_set - function that adds ana elemtn to the hash table
+ * @ht: pointer to table
+ * @key: key of the node
+ * @value: value of the node
+ * Return: return if the element has been added or no
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *current;
+	hash_node_t *temp;
 	unsigned long int index;
-	hash_node_t *new;
 
-	if (!key)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	new = malloc(sizeof(hash_node_t *));
-	if (!new)
-		return (0);
-
-	new->key = strdup(key);
-	if (!new->key)
-	{
-		free(new);
-		return (0);
-	}
-
-	new->value = strdup(value);
-	if (!new->value)
-	{
-		free(new->key);
-		free(new);
-		return (0);
-	}
 	index = key_index((const unsigned char *)key, ht->size);
-
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			if (!temp->value)
+				return (0);
+			return (1);
+		}
+		temp = temp->next;
+	}
+	current = create(key, value);
+	if (!current)
+		return (0);
 	if (ht->array[index])
 	{
-		hash_node_t *current = ht->array[index];
-		while (current != NULL)
-		{
-			if (strcmp(current->key, key) == 0)
-			{
-				free(current->key);
-				current->key = strdup(key);
-				return (1);
-			}
-			current = current->next;
-		}
-
-		if (strcmp(current->key, key) == 0)
-			new->next = current;
-		ht->array[index] = new;
+		current->next = ht->array[index];
+		ht->array[index] = current;
 	}
 	else
 	{
-		new->next = NULL;
-		ht->array[index] = new;
+		ht->array[index] = current;
 	}
 	return (1);
+}
+/**
+ * create - create a node with key and value
+ * @key: key of node
+ * @value: value of node
+ * Return: return the node
+*/
+hash_node_t *create(const char *key, const char *value)
+{
+	hash_node_t *current;
+
+	current = malloc(sizeof(hash_node_t));
+	if (!current)
+		return (NULL);
+
+	current->key = malloc(strlen(key) + 1);
+	if (!current->key)
+		return (NULL);
+	current->value = malloc(strlen(value) + 1);
+	if (!current->value)
+		return (NULL);
+	strcpy(current->key, key);
+	strcpy(current->value, value);
+	current->next = NULL;
+	return (current);
 }
