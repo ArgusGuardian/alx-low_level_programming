95d702692e52ae7ae16b85644b2245274e422348
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-01 03:50:59 +0100

    betty done

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,5 +1,9 @@
 #include "hash_tables.h"
-
+/**
+ * hash_djb2 - hash backend
+ *@str: strgin to hash
+ *Return: hashed string
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
