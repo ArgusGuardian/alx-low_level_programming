5e9d8d1dff9bc021d31665270ffd061fbb906f91
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-01 03:48:29 +0100

    typo

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->size = size;
 	table->array = malloc(size * sizeof(hash_node_t *));
 	if (!table->array)
-		return ();
+		return (NULL);
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 
