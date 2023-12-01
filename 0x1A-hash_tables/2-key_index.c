87dcd13e6180d491eb65b44ea055e812c087ace3
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-01 03:56:37 +0100

    2 DONE

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
-
+/**
+ * key_index - key to index
+ * @key: key to hash
+ * @size: size of table
+ * Return: return index
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int x;
