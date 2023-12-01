f4fb8cd5f8d1539af5177e7377611a6ebd64684f
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-01 03:45:11 +0100

    second commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,9 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_create - createa hashtable
+ *@size: size of the hash table
+ *Return: return pointer to the table or null
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned int i;
