3149b0e5635f4949589bad2b58cbf747a9ead102
Alaa Eddine <Alaaelhannaoui@proton.me>
2024-01-25 02:24:12 +0100

    sixth task done

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""this is a mearing function"""
+
+
+def island_perimeter(grid):
+    """this is a function to measure perimet of an island"""
+    x = len(grid[0])
+    
+    y = len(grid)
+
+    nobes = 0
+
+    s = 0
+
+    for i in range(y):
+        for j in range(x):
+            if grid[i][j] == 1:
+                s += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    nobes += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    nobes += 1
+    return s * 4 - nobes * 2
