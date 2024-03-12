93eabb7158e09ac57d2137add3e1f1a6dc0d98c4
Alaa Eddine <Alaaelhannaoui@proton.me>
2024-03-12 13:52:49 +0000

    betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,16 +8,18 @@
  * @value: value to search in
  * Return: Always EXIT_SUCCESS
  */
-int linear_search(int *array, size_t size, int value) {
-  int i = 0;
+int linear_search(int *array, size_t size, int value)
+{
+	int i = 0;
 
-  if (!array)
-    return (-1);
+	if (!array)
+		return (-1);
 
-  for (i = 0; i < (int)size; i++) {
-    printf("Value checked array[%u] = [%d]\n", i, array[i]);
-    if (array[i] == value)
-      return (i);
-  }
-  return (-1);
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
